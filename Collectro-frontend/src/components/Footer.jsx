cb1b9ee63511ea48af4cafb414f30405b38d15e8
Joe-Newm <j.newm500@gmail.com>
2025-10-10 21:15:13 -0500

    add genres to homepage

--- Collectro-frontend/src/components/Footer.jsx
+++ Collectro-frontend/src/components/Footer.jsx
@@ -9,6 +9,7 @@ export default function Footer() {
             <div className="flex items-center justify-center h-full flex-col">
                 <Link to="/" className="font-black text-xl text-white">COLLECTRO</Link>
                 <p>Created by Joseph Newman using the RAWG.io API</p>
+                <a target="_blank" className="underline" href="https://joseph-newman.com">joseph-newman.com</a>
             </div>
         </div>
     )
--- Collectro-frontend/src/layouts/HomePage.jsx
+++ Collectro-frontend/src/layouts/HomePage.jsx
@@ -4,6 +4,7 @@ import Card from '../components/Card';
 export default function HomePage() {
     const [newGames, setNewGames] = useState(null);
     const [featuredGames, setFeaturedGames] = useState(null);
+    const [genres, setGenres] = useState(null);
 
     // get now date plus last three months date for range of most played recent games
     const now = new Date();
@@ -14,20 +15,22 @@ export default function HomePage() {
 
 
   const apiKey = import.meta.env.VITE_RAWG_API_KEY;
-  const newGamesUrl = `https://api.rawg.io/api/games?key=${apiKey}&ordering=-released&platforms=4,187,18,1,7&stores=1,2,3,6&page_size=4`
+  const newGamesUrl = `https://api.rawg.io/api/games?key=${apiKey}&ordering=-released&platforms=4,187,18,1,7&stores=1,2,3,6&page_size=8`
   const featuredGamesUrl =  `https://api.rawg.io/api/games?key=${apiKey}` +
                             `&dates=${formatDate(threeMonthsAgo)},${formatDate(now)}` +
                             `&ordering=-added` +
                             `&platforms=4,187,18,1,186,7` +
                             `&stores=1,2,3,6` +
                             `&page_size=8`;
+    const genresUrl = `https://api.rawg.io/api/genres?key=${apiKey}`
 
     useEffect(() => {
         fetch(newGamesUrl).then((res) => res.json()).then((data) => setNewGames(data.results));
         fetch(featuredGamesUrl).then((res) => res.json()).then((data) => setFeaturedGames(data.results));
+        fetch(genresUrl).then((res) => res.json()).then((data) => setGenres(data.results));
     }, [])
    
-    if (!newGames || !featuredGames) return (<div className="flex h-screen justify-center items-center"><h2 className="">loading...</h2></div>)
+    if (!newGames || !featuredGames || !genres) return (<div className="flex h-screen justify-center items-center"><h2 className="">loading...</h2></div>)
 
     return (
         <div className="container mx-auto mt-10 flex flex-col">
@@ -40,6 +43,19 @@ export default function HomePage() {
                     </li>
                 ))}
             </ul> 
+            </div>
+            <div>
+            <h2 className="font-bold text-left mb-2 mt-10">New Releases</h2>
+            <ul className="grid grid-rows grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4 container mx-auto">
+                {genres.filter((genre) => genre.name !== "Educational").map((genre) => (
+                    <li key={genre.id} className="relative">
+                        <img src={genre.image_background} alt={genre.name} className="h-40 rounded-lg object-cover"/>
+                        <div className="bg-blue-500/50 rounded-lg absolute bottom-0 w-full h-full flex items-center justify-center">
+                            <h3 className="font-bold text-2xl">{genre.name}</h3>
+                        </div>
+                    </li>
+                ))}
+            </ul>
             </div>
             <div>           
             <h2 className="font-bold text-left mb-2 mt-10">New Releases</h2>
